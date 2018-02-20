93aa77339d8caedea558a72b55351d9223b5bdf5
ben <xy200362@163.com>
2018-02-21 01:34:39 +0800

    adjust setting for main network

--- SharderToken.sol
+++ SharderToken.sol
@@ -69,8 +69,8 @@ library SafeMath {
 */
 contract SharderToken {
     using SafeMath for uint;
-    string public constant NAME = "Sharder Storage Tester";
-    string public constant SYMBOL = "SST";
+    string public constant NAME = "Sharder Storage";
+    string public constant SYMBOL = "SS";
     uint public constant DECIMALS = 18;
     uint public totalSupply;
 
@@ -101,19 +101,17 @@ contract SharderToken {
     ///   +-----------------------------------------------------------------------------------+
     ///   | 250,000,000  |  50,000,000  |     50,000,000      |      None     |      None     |
     ///   +-----------------------------------------------------------------------------------+
-    ///   First Round supply 300,000,000 -> 3,000
-    uint256 internal constant FIRST_ROUND_ISSUED_SS = 3000;
+    uint256 internal constant FIRST_ROUND_ISSUED_SS = 300000000;
 
-    /// Max promotion 2,000,000 -> 20
-    uint256 internal constant MAX_PROMOTION_SS = 20;
+    /// Max promotion 0
+    uint256 internal constant MAX_PROMOTION_SS = 0;
 
     /// Maximum amount of fund to be raised, the sale ends on reaching this amount.
     /// We'll adjust hard cap in Feb. 21.
-    uint256 public constant HARD_CAP = 4 ether;
+    uint256 public constant HARD_CAP = 1500 ether;
 
-    /// Test network set to 1 ether = 5,000,000 SS -> 50
     /// We'll adjust rate base the 7-day average close price (Feb.15 through Feb.21, 2018) on CoinMarketCap.com at Feb.21.
-    uint256 public constant BASE_RATE = 50;
+    uint256 public constant BASE_RATE = 20521;
 
     /// We split the crowdsale into 2 phases.
     /// The real price for phase is `(1 + bonusPercentages[i]/100.0) * BASE_RATE`.
@@ -125,17 +123,16 @@ contract SharderToken {
 
     uint internal constant NUM_OF_PHASE = 2;
 
-    /// Each phase contains exactly 15250 Ethereum blocks, which is roughly 3 days,
+    /// Each phase contains exactly 78776 Ethereum blocks, which is roughly 15 days,
     /// See https://www.ethereum.org/crowdsale#scheduling-a-call
-    /// Test network set to 0.25 hour = 53 blocks, total time is 1 hour.
-    uint internal constant BLOCKS_PER_PHASE = 200;
+    uint internal constant BLOCKS_PER_PHASE = 78776;
 
     /// 1 ether == 1000 finney
-    /// Min contribution: 0.01 ether
-    uint256 public constant CONTRIBUTION_MIN = 10 finney;
+    /// Min contribution: 0.1 ether
+    uint256 public constant CONTRIBUTION_MIN = 100 finney;
 
-    /// Max contribution: 2 ether
-    uint256 public constant CONTRIBUTION_MAX = 2000 finney;
+    /// Max contribution: 5 ether
+    uint256 public constant CONTRIBUTION_MAX = 5000 finney;
 
     /// Crowdsale start block number.
     uint public saleStartAtBlock = 0;
