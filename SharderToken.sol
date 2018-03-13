1d51092edcf7eef0a5a19aca904f3317e4580b1d
kcinp <37299147+kcinp@users.noreply.github.com>
2018-03-13 07:19:02 +0100

    fixed safemath missing add() function
    - fixed safemath missing add() function
    - https://raw.githubusercontent.com/OpenZeppelin/zeppelin-solidity/master/contracts/math/SafeMath.sol

--- SharderToken.sol
+++ SharderToken.sol
@@ -63,6 +63,16 @@ library SafeMath {
     assert(b <= a);
     return a - b;
   }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
   
 /**
 * @title Sharder Token v2.0. SS (Sharder) is an upgrade from SS (Sharder Storage).
