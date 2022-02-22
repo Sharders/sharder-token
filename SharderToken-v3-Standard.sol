29a27000c6a98c8a9c3ea04b4b32644069f8bc42
ben <xy200362@163.com>
2022-02-22 10:52:09 +0800

    add SPDX declaration

--- SharderToken-v3-Standard.sol
+++ SharderToken-v3-Standard.sol
@@ -12,7 +12,9 @@
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-
+  
+  SPDX-License-Identifier: SimPL-2.0
+  
   Sharder-Token-v3
 */
 pragma solidity ^0.7.4;
